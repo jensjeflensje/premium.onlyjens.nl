bb5ca511833b02046abe1de229ab9c9d3fc6bbf6
Jens de Ruiter <jensgamesnl@gmail.com>
2023-12-24 18:31:00 +0100

    Add social icons

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -3,6 +3,11 @@
     <div class="header-content">
       <img id="logo" src="../assets/jens.png" alt="">
       <h3 id="title">OnlyJens Premium</h3>
+      <div class="social-container">
+        <a v-for="account in socialAccounts" :href="account.link" target="_blank">
+          <font-awesome-icon :icon="'fa-brands ' + account.icon"></font-awesome-icon>
+        </a>
+      </div>
     </div>
   </div>
   <ShadeBar/>
@@ -10,6 +15,13 @@
 
 <script setup lang="ts">
 import ShadeBar from '@/components/ShadeBar.vue'
+
+const socialAccounts = [
+  {icon: 'fa-github', link: 'https://github.com/jensjeflensje'},
+  {icon: 'fa-youtube', link: 'https://www.youtube.com/@jederu'},
+  {icon: 'fa-discord', link: 'https://discord.gg/U8DN52byBG'},
+  {icon: 'fa-linkedin', link: 'https://www.linkedin.com/in/jens-de-ruiter-7557551b1/'},
+]
 </script>
 
 <style scoped>
@@ -25,7 +37,7 @@ import ShadeBar from '@/components/ShadeBar.vue'
     display: inline-block;
     text-align: center;
     width: 100%;
-    padding: 48px 4px 96px 4px;
+    padding: 48px 4px 32px 4px;
   }
 
   #logo {
@@ -41,11 +53,34 @@ import ShadeBar from '@/components/ShadeBar.vue'
     margin: 0;
   }
 
+  .social-container {
+    margin-top: 36px;
+    display: flex;
+    gap: 12px;
+    justify-content: center;
+  }
+
+  svg { 
+    color: #f1f1ff;
+    font-size: 24px;
+    transition: all 300ms;
+  }
+
+  svg:hover {
+    transform: scale(1.1);
+    color: var(--blue-300);
+  }
+
   @media only screen and (max-width: 500px) {
     .header-content {
       backdrop-filter: blur(8px) saturate(0.4) contrast(1.2);
       -webkit-backdrop-filter: blur(8px) saturate(0.4) contrast(1.2);
       padding: 16px 4px;
+    }
+    
+    .social-container {
+      margin-top: 16px;
+      margin-bottom: 6px;
     }
   }
 
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,6 +5,13 @@ import 'primevue/resources/themes/lara-dark-blue/theme.css'
 import 'primeicons/primeicons.css'
 import './style.css'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faGithub, faYoutube, faDiscord, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+library.add(faGithub, faYoutube, faDiscord, faLinkedin)
+
 createApp(App)
     .use(PrimeVue)
+    .component('font-awesome-icon', FontAwesomeIcon)
     .mount('#app')
